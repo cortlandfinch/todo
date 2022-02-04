8c5f7586e95e1bdb1dcd39aeea3443ddf291db48
cortlandfinch <cortland.finch@gmail.com>
2022-02-04 14:59:58 -0500

    add function

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-var msg = 'bye World';
-console.log(msg);
+const msg = 'bye World';
+//console.log(msg);
+
+
+const msg2 = 'bye bye';
+console.log(msg2);
+
+
+
+
+function printByeWorld(){
+    console.log(msg);
+}
+
+function add(){
+    let result = 1 + 1;
+    return result;
+}
+
+
+printByeWorld();
+printByeWorld();
+const result = add();
+console.log(result);
